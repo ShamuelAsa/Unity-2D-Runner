d8c18b0c602dcb9a7e709e8ce0100ee22df47600
Skilious <ta_iha99@outlook.com>
2019-06-19 12:08:15 +1000

    Script & Background changes - Skilious
    The background has now been rearranged as a sprite with 2D box collider and a rigidbody2D.
    It has been scripted to have the infinite scrolling.
    If you wish to change the speed, its in the D_BackgroundScroll.
    
    DO NOT: Modify the RepeatingBackground.cs file.
    
    Thanks

---
+++ 2D Infinite Runner GPF/Assets/Scripts/D_BackgroundScroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_BackgroundScroller : MonoBehaviour
+{
+    private Rigidbody2D r_scroll;
+    private float speed = -1.5f;
+    private bool gameOver = false;
+	// Use this for initialization
+	void Start ()
+    {
+        r_scroll = GetComponent<Rigidbody2D>();
+
+
+        r_scroll.velocity = new Vector2(speed, 0);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(gameOver == true)
+        {
+            r_scroll.velocity = Vector2.zero;
+        }
+    }
+}
---
+++ 2D Infinite Runner GPF/Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour
+{
+    private BoxCollider2D B_Collider;
+    private float B_HorizontalLength;
+
+	private void Awake ()
+    {
+        B_Collider = GetComponent<BoxCollider2D>();
+        B_HorizontalLength = B_Collider.size.x;
+	}
+	
+
+	private void Update ()
+    {
+        if (transform.position.x < -B_HorizontalLength)
+        {
+            RepositionBackground();
+        }
+	}
+
+    private void RepositionBackground()
+    {
+        Vector2 offset = new Vector2 (B_HorizontalLength * 2f, 0);
+
+        transform.position = (Vector2) transform.position + offset;
+    }
+}
