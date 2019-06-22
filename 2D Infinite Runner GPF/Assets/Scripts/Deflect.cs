f3526144c84150a2057982f378c407472c13c4a5
Skilious <ta_iha99@outlook.com>
2019-06-22 22:17:25 +1000

    Added Attack and Deflect
    Those are the only controls that can be functionable later on.
    I only managed to add the controls in. Not bindable yet as we don't have a user interface to create a keybind section.

---
+++ 2D Infinite Runner GPF/Assets/Scripts/Deflect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deflect : MonoBehaviour
+{
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("Deflect Animation playing");
+        }
+    }
+
+    void OnTriggerEnter2D (Collider other)
+    {
+        if(other.gameObject.tag == "Robot" || other.gameObject.tag == "Laser")
+        {
+            if(Input.GetKeyDown(KeyCode.X))
+            {
+                Debug.Log("Deflected!");
+            }
+        }
+    }
+}
---
+++ 2D Infinite Runner GPF/Assets/Scripts/Player_Controls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controls : MonoBehaviour
+{
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            StartCoroutine(Attack());
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("Deflect Animation playing");
+        }
+    }
+    IEnumerator Attack()
+    {
+        gameObject.GetComponent<Collider2D>().isTrigger = true;
+        Debug.Log("Attack!");
+        yield return new WaitForSeconds(0.1f);
+        gameObject.GetComponent<Collider2D>().isTrigger = false;
+        Debug.Log("Attack animation ended!");
+    }
+}
