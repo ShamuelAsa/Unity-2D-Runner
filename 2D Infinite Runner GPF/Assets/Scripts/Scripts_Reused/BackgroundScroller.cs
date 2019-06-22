f0c63128123c6a1b083c64c8da403c5b0769a3a1
Skilious <ta_iha99@outlook.com>
2019-06-22 22:22:50 +1000

    Reverted the Background Scroll
    I have decided to keep the first Background Scroll as we might keep it since we don't need to worry about Resolution Ratios.
    We'll support this game only for 16:9 for development purposes.

---
+++ 2D Infinite Runner GPF/Assets/Scripts/Scripts_Reused/BackgroundScroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(Time.time * speed, 0);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
+
+//Please do not remove this until I can confirm the usage of the other background scroller or deciding to use this as our last resort.
+
+//Reason: Resolution change to a smaller resolution will cause the character to disappear. Otherwise, we stick to 16:9 ratio resolution and keep this script. 
