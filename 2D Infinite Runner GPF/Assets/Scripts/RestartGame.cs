a410058b8dc6a32491d65299b1e465179d0f0947
Skilious <ta_iha99@outlook.com>
2019-07-01 20:17:36 +1000

    Updated!
    Restarts when the enemy drone flies above the character.
    This prevents an endless flying once the enemy crosses the character.

---
+++ 2D Infinite Runner GPF/Assets/Scripts/RestartGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class RestartGame : MonoBehaviour
+{
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
